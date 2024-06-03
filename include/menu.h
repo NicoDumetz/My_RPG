03f86fbb4cc773e698d8b1a3f1a90cb1e2c43105
NicoDumetz <nicolas.dumetz@epitech.eu>
2024-06-03 14:59:37 +0200

    REFACTOR: initiation of texture and font for the menu and the parameter menu, now in the file set_all_texture/font

--- include/menu.h
+++ include/menu.h
@@ -11,6 +11,7 @@
 
 typedef struct menu_s {
     int pos;
+    int save;
     sfSprite *background;
     button_t *play;
     button_t *new;
@@ -18,17 +19,6 @@ typedef struct menu_s {
     button_t *param;
     button_t *quit;
 } menu_t;
-
-typedef struct ingame_menu_s {
-    int from;
-    int pos;
-    sfSprite *background;
-    button_t *resume;
-    button_t *save;
-    button_t *load_save;
-    button_t *param;
-    button_t *quit;
-} ingame_menu_t;
 
 menu_t *create_menu_struct(rpg_t *rpg);
 void destroy_menu(menu_t *menu);
