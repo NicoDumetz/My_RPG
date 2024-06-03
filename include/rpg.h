a44dabe225112f50ea8ff76f0343756d42a3b677
NicoDumetz <nicolas.dumetz@epitech.eu>
2024-06-03 15:02:04 +0200

    fix : wrong proto is_champ func

--- include/rpg.h
+++ include/rpg.h
@@ -102,6 +102,10 @@ typedef enum texture_type_e {
     BUTTON_PLUS_PRESSED_E,
     BUTTON_MINUS_E,
     BUTTON_MINUS_PRESSED_E,
+    RESUME_E,
+    RESUME_PRESSED_E,
+    RESUME_HOVER_E,
+    MENU_INGAME_E,
     INVENTORY_TEXT,
     INVENTORY_SLOT_TEXT,
     BACK_INVENTORY_HERO,
@@ -243,6 +247,7 @@ void appli_save_quest(rpg_t *rpg, save_data_t *save);
 void rpg(rpg_t *rpg);
 void destroy_rpg(rpg_t *rpg);
 rpg_t *create_rpg_struct(void);
+void reinitalize_rpg(rpg_t *rpg);
 void manage_heros(heros_t *heros, rpg_t *rpg);
 void set_all_texture(sfTexture **text_tab);
 void set_all_font(sfFont **font_tab);
@@ -251,7 +256,7 @@ int check_asset(sfTexture **text_tab, sfFont **font_tab);
 /**EVENT**/
 void manage_event(rpg_t *rpg);
 void manage_movements(rpg_t *rpg);
-void manage_heros_attack(heros_t *heros, bool *key_code, rpg_t *rpg);
+void manage_heros_attack(heros_t *heros, bool *key_code);
 sfSprite *wich_back(rpg_t *rpg);
 sfImage *wich_img(rpg_t *rpg);
 void manage_right(rpg_t *rpg);
--- src/attack/heros/manage_heros_attack.c
+++ src/attack/heros/manage_heros_attack.c
@@ -7,31 +7,31 @@
 
 #include "rpg.h"
 
-int which_attack(heros_t *heros, bool *key_code, rpg_t *rpg)
+int which_attack(heros_t *heros, bool *key_code)
 {
     if (key_code[sfKeyI]) {
         heros->npc->act_action = ATTACK_F;
-        return launch_sword_song(rpg);
+        return 1;
     }
     if (key_code[sfKeyK]) {
         heros->npc->act_action = ATTACK_B;
-        return launch_sword_song(rpg);
+        return 1;
     }
     if (key_code[sfKeyJ]) {
         heros->npc->act_action = ATTACK_L;
-        return launch_sword_song(rpg);
+        return 1;
     }
     if (key_code[sfKeyL]) {
         heros->npc->act_action = ATTACK_R;
-        return launch_sword_song(rpg);
+        return 1;
     }
     return 0;
 }
 
-void manage_heros_attack(heros_t *heros, bool *key_code, rpg_t *rpg)
+void manage_heros_attack(heros_t *heros, bool *key_code)
 {
     if (heros->stamina > 15 && heros->npc->is_attack == false
-        && which_attack(heros, key_code, rpg) == 1){
+        && which_attack(heros, key_code) == 1){
         heros->npc->entity->rect_sprite.left = 0;
         heros->stamina -= SWORD_COST;
         heros->npc->is_attack = true;
